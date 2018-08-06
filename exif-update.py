5cebe605443965337d116fdac29b24e7e8c0b575
David Janes <david@consensas.com>
2018-08-06 18:06:44 -0400

    find the location

--- exif-update.py
+++ exif-update.py
@@ -6,8 +6,8 @@
 #
 
 import sys
-import path
 import os
+import pprint
 import optparse
 import yaml
 import piexif
@@ -38,6 +38,15 @@ parser.add_option(
 class OptionException(Exception):
     pass
 
+def help(message):
+    if message:
+        print >> sys.stderr, "%s: %s\n" % ( sys.argv[0], message, )
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    else:
+        parser.print_help(sys.stdout)
+        sys.exit(0)
+
 try:
     if not args:
         raise   OptionException, "at least one filename required"
@@ -47,3 +56,72 @@ except OptionException, s:
     sys.exit(1)
 
 ## load configuration
+configuration = {
+    "locations": []
+}
+
+for p in [ ".exif-update.yaml", os.path.expanduser("~/.exif-update.yaml"), ]:
+    try:
+        data = yaml.load(file(p))
+    except IOError:
+        continue
+
+    for key, value in data.items():
+        if not configuration.get(key):
+            configuration[key] = value
+        elif isinstance(value, dict) and isinstance(configuration[key], dict):
+            configuration[key] = configuration[key].update(value)
+        elif isinstance(value, list) and isinstance(configuration[key], dict):
+            configuration[key] = configuration[key].extend(value)
+        else:
+            configuration[key] = value
+
+## narrow down the location
+def parse_geo(v):
+    if not v:
+        return
+
+    parts = v.split(",")
+    if len(parts) != 2:
+        return 
+
+    try:
+        return map(float, parts)
+    except:
+        return
+
+location = None
+
+if options.location:
+    geo = parse_geo(options.location)
+    if geo:
+        location = geo
+    else:
+        for l in configuration["locations"]:
+            if l.get("name") == options.location:
+                break
+        else:
+            l = None
+
+        if not l:
+            help("location '%s' not found in database" % options.location)
+
+        geo = l.get("geo")
+        if not geo:
+            help("location '%s in database didn't have 'geo'" % options.location)
+
+        geo = parse_geo(geo)
+        if not geo:
+            help("location '%s in database didn't have valid 'geo'" % options.location)
+
+        location = geo
+
+print location
+"""
+ls = configuration["locations"]
+ls = filter(lambda l: l.get("name"), ls)
+ls = dict(zip(map(lambda l: l.get("name"), ls), ls))
+configuration["locations"] = ls
+
+pprint.pprint(configuration)
+"""
